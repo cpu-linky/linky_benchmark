eb8d2fff677ff1eb3084c24950482a449e142bf5
Pallandos <commit@pallandos.dev>
2026-01-19 15:44:45 +0100

    fix: change type to uint64

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "src/load/load.h"
 
 int main(int, char**){
     printf("Hello, from cstate_monitor!\n");
 
-    int a = 81992911;
+    uint64_t a = 4307736511339693217;
     if (is_prime(a) == 1){
-        printf("%d is prime \n", a);
+        printf("%li is prime \n", a);
     } 
     else {
-        printf("%d is not prime \n", a);
+        printf("%li is not prime \n", a);
     }
 }
--- src/load/cpu_load.c
+++ src/load/cpu_load.c
@@ -1,13 +1,14 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdint.h>
 
-int is_prime(int a){
+int is_prime(uint64_t a){
     // easy and trivial primality test
 
-    double sqrt_a;
-    sqrt_a = sqrt(a);
+    uint64_t sqrt_a;
+    sqrt_a = (uint64_t)sqrt(a);
     
-    for (int i = 2; i < sqrt_a; i++){
+    for (uint64_t i = 2; i <= sqrt_a; i++){
         if (a % i == 0){
             return 0;
         }
