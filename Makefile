658355b5f84102e92d6a8cb89f86c54007bb49ef
Pallandos <commit@pallandos.dev>
2026-01-25 11:57:40 +0100

    feat: all tests are now done from one file

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ SRC_DIR= src
 
 BINS = $(BIN_DIR)/cpu_load \
 	   $(BIN_DIR)/memory_load \
-	   $(BIN_DIR)/io_load
+	   $(BIN_DIR)/io_load  \
+	   $(BIN_DIR)/cstate_monitor
 
 .PHONY: clean 
 
@@ -17,7 +18,7 @@ clean:
 make_dirs:
 	@mkdir -p $(BIN_DIR)
 
-# ============================================================
+# ========================= main =============================
 
 $(BIN_DIR)/cpu_load: $(SRC_DIR)/load/cpu_load.c
 	gcc -o $@ $(SRC_DIR)/load/cpu_load.c -lm
@@ -27,5 +28,8 @@ $(BIN_DIR)/memory_load: $(SRC_DIR)/load/memory_load.c
 
 $(BIN_DIR)/io_load: $(SRC_DIR)/load/io_load.c
 	gcc -o $@ $(SRC_DIR)/load/io_load.c
+
+$(BIN_DIR)/cstate_monitor: main.c
+	gcc -o $@ main.c -lm
 
 all: make_dirs $(BINS)
--- main.c
+++ main.c
@@ -1,9 +1,53 @@
+#include <unistd.h>
+#include <sys/wait.h>
 #include <stdio.h>
-#include <stdint.h>
-#include "src/load/load.h"
 
-int main(int, char**){
-    int n = 100000000;
-    printf("Counting how many prime below %d\n", n);
-    prime_counting(n);
+int main() {
+    char *command_cpu[] = {"turbostat", "--quiet", "--interval", "1", "bin/cpu_load", "10000000", NULL};
+    char *command_mem[] = {"turbostat", "--quiet", "--interval", "1", "bin/memory_load", "10000000", NULL};
+    char *command_io[] = {"turbostat", "--quiet", "--interval", "1", "bin/io_load", "300", NULL};
+
+    // Fork 1 : CPU Load
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(command_cpu[0], command_cpu);
+        perror("[ERROR] execvp failed for CPU load");
+        return 1;
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("[SUCCESS] CPU load test done! (Exit code : %d)\n", WEXITSTATUS(status));
+    } else {
+        perror("[ERROR] Fail to fork CPU load");
+    }
+
+    // Fork 2 : memory load
+    pid = fork();
+    if (pid == 0) {
+        execvp(command_mem[0], command_mem);
+        perror("[ERROR] execvp failed for memory load");
+        return 1;
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("[SUCCESS] Memory load test done! (Exit code : %d)\n", WEXITSTATUS(status));
+    } else {
+        perror("[ERROR] Fail to fork Memory load");
+    }
+
+    // Fork 3 : io load
+    pid = fork();
+    if (pid == 0) {
+        execvp(command_io[0], command_io);
+        perror("[ERROR] execvp failed for io load");
+        return 1;
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+        printf("[SUCCESS] IO load test done! (Exit code : %d)\n", status);
+    } else {
+        perror("[ERROR] Fail to fork io load");
+    }
+
+    return 0;
 }
